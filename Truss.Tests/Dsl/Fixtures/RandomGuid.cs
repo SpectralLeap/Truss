a7132e55504ecfb8e59e0c1b5d0b0e6d846cea90
henningfutrell <henning.futrell@protonmail.com>
2024-01-03 18:04:18 -0600

    Add empty guid for testing

--- Truss.Tests/Dsl/Fixtures/RandomGuid.cs
+++ Truss.Tests/Dsl/Fixtures/RandomGuid.cs
@@ -1,6 +1,16 @@
 namespace Truss.Tests.Dsl.Fixtures;
 
-public sealed class RandomGuid
+public interface IGuidProvider
 {
-    public Guid Guid = Guid.NewGuid();
+    public Guid Guid { get; }
+}
+
+public sealed class RandomGuid : IGuidProvider
+{
+    public Guid Guid => Guid.NewGuid();
+}
+
+public sealed class EmptyGuid : IGuidProvider
+{
+    public Guid Guid => Guid.Empty;
 }
