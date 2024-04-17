bea965ccc6417bd6f4e7c902726bfe78b764bc52
henningfutrell <henning.futrell@protonmail.comm>
2024-04-17 18:11:05 -0500

    Add files

---
+++ Truss.Modeling.Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Truss.Modeling.Application.Cqrs.Commands;
+using Truss.Modeling.Application.Cqrs.EventSourcing.Writing;
+using Truss.Modeling.Application.Cqrs.Queries;
+using Truss.Modeling.Application.DomainEvents;
+
+namespace Truss.Modeling.Infrastructure;
+
+public static class ServiceExtensions
+{
+    public static void AddTruss(
+        this IServiceCollection services,
+        Assembly[] assemblies
+    )
+    {
+        var model = new TrussDependencyModel(
+            services,
+            assemblies
+        );
+
+        model.CloseAllTypesOf(typeof(IDomainEventHandler<>));
+        model.CloseAllTypesOf(typeof(ICommandHandler<>));
+        model.CloseAllTypesOf(typeof(IQueryHandler<,>));
+        model.CloseAllTypesOf(typeof(IChangeEventHandler<>));
+    }
+}
