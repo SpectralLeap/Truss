8544a65d5759ca55baf492308d63223009727a0e
hfutrell-gss <hfutrell@gssmail.com>
2024-04-16 21:28:41 -0500

    Fix dependency issues

--- publish.sh
+++ publish.sh
@@ -2,37 +2,25 @@
 
 output_directory=".nuget_output"
 mkdir -p "$output_directory"
-rm -rf $output_directory/refs
 rm $output_directory/*
 
-projects=(
-    "Truss.Monads.Results"
-    "Truss.Monads.Results.Protos"
-    "Truss.Monads.Results.Extensions.Fluent"
-    "Truss.Modeling.Domain"
-    "Truss.Modeling.Application"
-    "Truss.Testing"
-    "Truss.Testing.AspNetCore"
-)
-
-for project in "${projects[@]}"
-do
-    echo "Building $project..."
-    dotnet build "$project/$project.csproj" --configuration Release --output $output_directory
-    
-    if [ $? -ne 0 ]; then
-        echo "Build failed for $project"
-        exit 1
-    fi
-done
+if uname -r | grep -q Microsoft; then
+    echo "Running on WSL"
+    powershell.exe dotnet build --configuration Release
+    powershell.exe dotnet pack -o $output_directory
+else
+    echo "Running on Linux"
+    dotnet build --configuration Release
+    dotnet pack -o $output_directory
+fi
 
 api_key=$NUGET_API_KEY
-server_url=$NUGET_SERVER_URL
+server=$NUGET_SERVER
 
 find $output_directory -name '*.nupkg' | while read -r package_file
 do
-    echo "Pushing $package_file to $server_url..."
-    dotnet nuget push "$package_file" --source "$server_url" --api-key "$api_key"
+    echo "Pushing $package_file to $server..."
+    dotnet nuget push "$package_file" --source "$server" --api-key "$api_key"
 done
 
 echo "Build and push process for latest packages completed."
