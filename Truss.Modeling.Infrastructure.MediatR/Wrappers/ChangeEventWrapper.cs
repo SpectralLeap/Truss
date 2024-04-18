16c47eb260e0b9e81ddc70055d4e0d10eb65697d
henningfutrell <henning.futrell@protonmail.com>
2024-04-17 21:58:35 -0500

    Remove defunct projects

--- Truss.Modeling.Infrastructure.MediatR/Wrappers/ChangeEventWrapper.cs
+++
@@ -1,8 +0,0 @@
-using MediatR;
-
-namespace Truss.Modeling.Infrastructure.MediatR.Wrappers;
-
-internal sealed class ChangeEventWrapper<TChangeEvent>(TChangeEvent changeEvent) : INotification
-{
-    public TChangeEvent ChangeEvent { get; } = changeEvent;
-}
